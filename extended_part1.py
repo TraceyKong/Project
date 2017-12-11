b436d24e0c55f184005ff7dc5b8c2290cf248361
TraceyKong <zixuan.kong70@myhunter.cuny.edu>
2017-12-11 11:43:46 -0500

    add or

--- extended_part1.py
+++ extended_part1.py
@@ -23,14 +23,17 @@ print (and_query("Inc","Church"))
 
 def or_query(key1,key2):
 	if project.valid_key(key1) or project.valid_key(key2):
-		return or_key(key1,key2,"MOCK_DATA.csv")
+		k1 = project.mock_data_dic[key1]
+		k2 = project.mock_data_dic[key2]
+		return or_key(k1,k2,"MOCK_DATA.csv")
 	else:
 		return "No result."
 
 def or_key(k1,k2,filename):
-	l = project.return_values(filename, k1)
-	l.extend(project.return_values(filename,k2))
-	return l
+	f = project.file_reader(filename)
+	l = k1
+	l.extend(k2)
+	l=set(l)
+	return [f[int(i)] for i in l]
 
 a = or_query("Inc", "Church")
-print(a)
