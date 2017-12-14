40f79096506dd605a6e72394571c72030af47a84
TraceyKong <zixuan.kong70@myhunter.cuny.edu>
2017-12-13 21:50:20 -0500

    added narrow search and home page

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hello"
+    return render_template("home.html")
 
 @app.route("/input",methods=["POST","GET"])
 def index():
+	md = p.mock_data[1:]
 	if request.method=="GET":
-		return render_template("inverted_index.html",result = p.mock_data[1:])
+		return render_template("inverted_index.html",result = md, lresult = str(len(md))+'/'+str(len(md))+' results')
 	term = request.form['term']
-	if term=='': return render_template("inverted_index.html",result = p.mock_data[1:])
+	if not term.strip(): return render_template("inverted_index.html",nresult = "No result for '"+term+"' please enter again.",lresult = '0/'+str(len(md))+' results')
 	t = p.clean_text(term).title().split()
 	result = []
 	if len(term)>1:
@@ -22,9 +23,30 @@ def index():
 				return render_template("inverted_index.html",
 					nresult = "No result for '"+term+"', please enter again.")
 			result = e1.and_query_1(result,i)
-		return render_template("inverted_index.html",result = p.return_values(result))
+		result = p.return_values(result)
+		return render_template("inverted_index.html",result = result, lresult = str(len(result))+'/'+str(len(md))+' results')
 	elif p.valid_key(t[0]):
 		return render_template("inverted_index.html",result = p.return_values(p.mock_data_dic[t[0]]))
- 	return render_template("inverted_index.html",nresult = "No result for '"+term+"', please enter again.")
+ 	return render_template("inverted_index.html",nresult = "No result for '"+term+"', please enter again.",lresult = '0/'+str(len(md))+' results')
 
+@app.route("/input/narrow_search",methods=["POST","GET"])
+def narrow():
+	md = p.mock_data[1:]
+	if request.method=="GET":
+		return render_template("narrow_search.html",result = md, lresult = str(len(md))+'/'+str(len(md))+' results')
+	term = request.form['term']
+	if not term.strip(): return render_template("inverted_index.html",nresult = "No result for '"+term+"' please enter again.",lresult = '0/'+str(len(md))+' results')
+	t = p.clean_text(term).title().split()
+	result = []
+	if len(term)>1:
+		for i in t:
+			if not p.valid_key(i): 
+				return render_template("inverted_index.html",
+					nresult = "No result for '"+term+"', please enter again.")
+			result = e1.and_query_1(result,i)
+		result = p.return_values(result)
+		return render_template("inverted_index.html",result = result, lresult = str(len(result))+'/'+str(len(md))+' results')
+	elif p.valid_key(t[0]):
+		return render_template("inverted_index.html",result = p.return_values(p.mock_data_dic[t[0]]))
+ 	return render_template("inverted_index.html",nresult = "No result for '"+term+"', please enter again.",lresult = '0/'+str(len(md))+' results')
 app.run(debug=True,host="0.0.0.0",port=5000)
